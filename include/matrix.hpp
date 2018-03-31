114b6c61637bfa098b831e25c8830a5797e9c0c4
Libo Wang <lw2768@columbia.edu>
2018-03-31 16:42:06 -0400

    blah

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,9 +1,19 @@
+#ifndef _INCLUDE_MATRIX_HPP
+#define _INCLUDE_MATRIX_HPP
+
+#include <array>
+
 namespace lal {
     typedef int index_t;
 
     template <typename NumericType, index_t Rows, index_t Cols>
     class matrix {
+    public:
         matrix();
         ~matrix();
+    private:
+        std::array<std::array<NumericType, Cols>, Rows> _base;
     };
 }
+
+#endif
