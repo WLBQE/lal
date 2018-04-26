19cc23c5bede83e1b8bcaf25f49314ba52c78e9e
JYW <overflow@JYWdeMacBook-Pro.local>
2018-04-26 10:16:02 -0400

    bug report

--- test_bug.cpp
+++ test_bug.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
-#include <array>
+#include <vector>>
 #include <assert.h>
 #include "include/dmatrix.hpp"
 
 int main()
 {
     lal::dynamic_matrix<double> dmat1(3, 3, 1);
-    assert(dmat1.at(0, 0) == 1);
-    assert(dmat1.at(2, 2) == 1);
-
-    auto it = dmat1.col_end() - 3; //pass
-    std::cout << *it << "\n";
-
-    auto it2 = dmat1.col_begin() + 8;
-    it2 = dmat1.col_end() - 3; //fail
-    std::cout << *it << "\n";
+    std::cout << dmat1 << "\n";
+    dmat1.insert_column(0, std::vector<double>(3,0));
+    std::cout << dmat1 << "\n";
+    
     return 0;
 }
