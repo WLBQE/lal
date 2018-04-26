f69b61142cc48143d8b75a48fc18eeee3581d172
Libo Wang <lw2768@columbia.edu>
2018-04-26 16:56:55 -0400

    changed demo

--- constexpr_demo.cpp
+++ constexpr_demo.cpp
@@ -2,8 +2,8 @@
 #include "include/matrix.hpp"
 
 int main() {
-    lal::matrix<int, 3, 3> m1 {{1, 2, 3, 4, 5, 6, 7, 8, 9}};
-    lal::matrix<int, 3, 3> m2 {{9, 8, 7, 6, 5, 4, 3, 2, 1}};
+    lal::matrix<int, 2, 3> m1 {{1, 2, 3, 4, 5, 6}};
+    lal::matrix<int, 3, 2> m2 {{6, 5, 4, 3, 2, 1}};
     for (auto& val : m1 * m2)
         printf("%d\n", val);
     return 0;
