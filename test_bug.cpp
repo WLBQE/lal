4d91ac6ab5e019c6d7dfe7688c70345dfdce9c3f
JYW <overflow@JYWdeMBP.fios-router.home>
2018-04-25 23:30:55 -0400

    bug report

--- test_bug.cpp
+++ test_bug.cpp
@@ -8,10 +8,12 @@ int main()
     lal::dynamic_matrix<double> dmat1(3, 3, 1);
     assert(dmat1.at(0, 0) == 1);
     assert(dmat1.at(2, 2) == 1);
-    lal::dynamic_matrix<int> dmat2(3, 3, 1);
-    assert(dmat2.at(0, 0) == 1);
-    assert(dmat2.at(2, 2) == 1);
-    std::cout << dmat2 << "\n";
-    assert(dmat2 == dmat1);
+
+    auto it = dmat1.col_end() - 3; //pass
+    std::cout << *it << "\n";
+
+    auto it2 = dmat1.col_begin() + 8;
+    it2 = dmat1.col_end() - 3; //fail
+    std::cout << *it << "\n";
     return 0;
 }
